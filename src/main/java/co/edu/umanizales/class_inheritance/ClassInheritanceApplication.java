229dfe181e4952aad2e98205e0e2e6e23c5a55c7
JuanRicon <1024juandi@gmail.com>
2024-10-18 20:20:18 -0500

    initial commit

---
+++ src/main/java/co/edu/umanizales/class_inheritance/ClassInheritanceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.class_inheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassInheritanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassInheritanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/umanizales/class_inheritance/ClassInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.class_inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
